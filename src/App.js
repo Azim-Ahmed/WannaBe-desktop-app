3042481ded4807a65f743045486a36daee7562fb
Azim Uddin Ahamed <azim@sqbear.com.au>
2021-09-21 00:15:23 +0600

    changes and added my first desktop application with react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+//
+import ReactWeather, { useOpenWeather } from 'react-open-weather';
+
+
 
 function App() {
+  const { data, isLoading, errorMessage } = useOpenWeather({
+    key: 'a4d87bf2001ef9bdc8330b755d6cc87d',
+    lat: '1.2921',
+    lon: '36.8219',
+    lang: 'en',
+    unit: 'metric', // values are (metric, standard, imperial)
+  });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App">
+        <ReactWeather
+          isLoading={isLoading}
+          errorMessage={errorMessage}
+          data={data}
+          lang="en"
+          locationLabel="Nairobi"
+          unitsLabels={{ temperature: 'C', windSpeed: 'Km/h' }}
+          showForecast
+        />
+      </div>
     </div>
   );
 }
